f8127cd652fa09808802ddcb1bade7aea7f05be0
Bookdly Dev <dev@bookdly.co.uk>
2026-07-08 11:23:01 +0200

    Remove Customer panel URL field from Share your page modal

--- plugins/booknetic-customer-panel/App/Listener.php
+++ plugins/booknetic-customer-panel/App/Listener.php
@@ -15,14 +15,7 @@ class Listener
 
     public static function saasSharePageFooter()
     {
-        if( Helper::getOption('customer_panel_enable', 'off', false) == 'on' ):?>
-		<div class="form-row">
-			<div class="form-group col-md-12">
-				<label for="input_customer_cabinet_url"><?php echo bkntc__('Customer panel URL')?>:</label>
-				<input type="text" id="input_customer_cabinet_url" readonly class="form-control" value="<?php echo CustomerPanelHelper::customerPanelURL()?>">
-			</div>
-		</div>
-		<?php endif;
+        // Removed as it is not needed in Share Page modal
     }
 
     public static function initGutenbergBlocks()
